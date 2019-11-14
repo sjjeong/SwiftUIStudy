1344e9e4a9eeff8c0fac5734e93bf10baf33058f
sjjeong <sjjeong1225@gmail.com>
2019-11-14 20:55:58 +0900

    Section 2, Create the Row View
    https://developer.apple.com/tutorials/swiftui/building-lists-and-navigation#create-the-row-view

---
+++ Landmarks/Landmarks/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by SeokJun Jeong on 2019/11/14.
+//  Copyright © 2019 dino. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50.0, height: 50.0)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
