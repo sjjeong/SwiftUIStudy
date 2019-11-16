570c504ea0de5ac5d020b91614e5571374cfac3a
sjjeong <sjjeong1225@gmail.com>
2019-11-16 20:01:55 +0900

    Section 3, Add a Control to Toggle the State
    https://developer.apple.com/tutorials/swiftui/handling-user-input#add-a-control-to-toggle-the-state

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -9,14 +9,20 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State var showFavoriteOnly = false
+    @State var showFavoriteOnly = true
     
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                if !self.showFavoriteOnly || landmark.isFavorite {
-                    NavigationLink(destination: LandmarkDetail(landmark: landmark)){
-                        LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorite only")
+                }
+                
+                ForEach(landmarkData) { landmark in
+                    if !self.showFavoriteOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                            LandmarkRow(landmark: landmark)
+                        }
                     }
                 }
             }
